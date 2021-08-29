d09f5bf424e26023fbc8d9ce8bcb94d578cad74c
Benjamin Ashman <hello@benashman.me>
2021-08-30 00:50:37 +0100

    Make exercise search case insensitive

--- Lifts/FilteredExerciseList.swift
+++ Lifts/FilteredExerciseList.swift
@@ -19,7 +19,7 @@ struct FilteredExerciseList: View {
         fetchRequest = FetchRequest<Exercise>(
             entity: Exercise.entity(),
             sortDescriptors: [],
-            predicate: NSPredicate(format: "name BEGINSWITH %@", filter))
+            predicate: NSPredicate(format: "name BEGINSWITH[c] %@", filter))
         
         _showingAddEntrySheet = showingAddEntrySheet
     }
