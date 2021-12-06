2289572ee90060c555d8919a267ba5b3aa29c8b7
Benjamin Ashman <benjaminashman@Benjamins-MacBook-Pro.local>
2021-12-06 14:56:34 -0800

    Green add button

--- Lifts/Views/HomeView.swift
+++ Lifts/Views/HomeView.swift
@@ -35,12 +35,9 @@ struct HomeView: View {
                             Section(header: DateSectionHeader(date: groupedEntries(entries)[section][0].timestamp!)) {
                                 ForEach(groupedEntries(entries)[section], id: \.self) { entry in
                                     EntryRow(entry: entry)
-                                    Button {
-                                        deleteEntry(entry)
-                                    } label: {
-                                        Text("Delete")
-                                    }
-
+                                        .onDelete(perform: {
+                                            deleteEntry(entry)
+                                        })
                                 }
                             }
                         }
@@ -118,6 +115,8 @@ struct HomeView: View {
             print("deleting entry: ")
             print(entry)
             
+            viewContext.delete(entry)
+            
             do {
                 try viewContext.save()
             } catch {
@@ -149,9 +148,10 @@ struct AddButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .frame(width: 64, height: 64)
-            .foregroundColor(Color.white)
-            .background(Color(red: 254/255, green: 54/255, blue: 94/244, opacity: 1.0))
-            .clipShape(RoundedRectangle(cornerRadius: 24, style: .continuous))
+            .foregroundColor(Color.black)
+            .background(Color(red: 166/255, green: 254/255, blue: 7/255, opacity: 1.0))
+//            .clipShape(RoundedRectangle(cornerRadius: 24, style: .continuous))
+            .cornerRadius(32.0)
             .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
             .animation(.spring(), value: configuration.isPressed)
     }
@@ -162,4 +162,94 @@ struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
+}
+
+struct Delete: ViewModifier {
+    
+    let action: () -> Void
+    
+    @State var offset: CGSize = .zero
+    @State var initialOffset: CGSize = .zero
+    @State var contentWidth: CGFloat = 0.0
+    @State var willDeleteIfReleased = false
+   
+    func body(content: Content) -> some View {
+        content
+            .background(
+                GeometryReader { geometry in
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.red)
+                        Image(systemName: "trash")
+                            .foregroundColor(.white)
+                            .font(.title2.bold())
+                            .layoutPriority(-1)
+                    }.frame(width: -offset.width)
+                    .offset(x: geometry.size.width)
+                    .onAppear {
+                        contentWidth = geometry.size.width
+                    }
+                    .gesture(
+                        TapGesture()
+                            .onEnded {
+                                delete()
+                            }
+                    )
+                }
+            )
+            .offset(x: offset.width, y: 0)
+            .gesture (
+                DragGesture()
+                    .onChanged { gesture in
+                        if gesture.translation.width + initialOffset.width <= 0 {
+                            self.offset.width = gesture.translation.width + initialOffset.width
+                        }
+                        if self.offset.width < -deletionDistance && !willDeleteIfReleased {
+                            hapticFeedback()
+                            willDeleteIfReleased.toggle()
+                        } else if offset.width > -deletionDistance && willDeleteIfReleased {
+                            hapticFeedback()
+                            willDeleteIfReleased.toggle()
+                        }
+                    }
+                    .onEnded { _ in
+                        if offset.width < -deletionDistance {
+                            delete()
+                        } else if offset.width < -halfDeletionDistance {
+                            offset.width = -tappableDeletionWidth
+                            initialOffset.width = -tappableDeletionWidth
+                        } else {
+                            offset = .zero
+                            initialOffset = .zero
+                        }
+                    }
+            )
+            .animation(.interactiveSpring())
+    }
+    
+    private func delete() {
+        offset.width = -contentWidth
+        action()
+    }
+    
+    private func hapticFeedback() {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+    }
+    
+    //MARK: Constants
+    
+    let deletionDistance = CGFloat(200)
+    let halfDeletionDistance = CGFloat(50)
+    let tappableDeletionWidth = CGFloat(100)
+    
+    
+}
+
+extension View {
+    
+    func onDelete(perform action: @escaping () -> Void) -> some View {
+        self.modifier(Delete(action: action))
+    }
+    
 }
